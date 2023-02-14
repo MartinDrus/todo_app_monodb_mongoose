932a994a07fb775d099ee84476b47d6c6cf0cd29
Martin <martindrus@outlook.de>
2023-02-14 09:36:40 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import cors from 'cors';
+import config from './src/service/config.js'
+
+const app = express();
+
---
+++ src/service/config.js
@@ -0,0 +1,7 @@
+
+
+const config = {
+    apiPort: 8080
+}
+
+export default config;
